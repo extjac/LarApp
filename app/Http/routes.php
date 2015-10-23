baf28ab2bcd9e7baf2e5d3394fe0991585578df4
Javier C <jciocci@outlook.com>
2015-10-23 15:41:35 -0400

    added router

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,67 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+ Route::get('/home', function () {
+    return view('home');
+});
+
+
+/** 
+* patterns
+*/
+Route::pattern('id', '[0-9]+');
+
+
+/** 
+* AUTH
+*/
+Route::get('/auth/register' , 'Auth\AuthController@getRegister');
+Route::get('/auth/login' 	, 'Auth\AuthController@getLogin');
+Route::get('/auth/logout' 	, 'Auth\AuthController@getLogout');
+Route::post('/auth/register', 'Auth\AuthController@postRegister');
+Route::post('/auth/login' 	, 'Auth\AuthController@postLogin');
+
+
+//if Logged in
+Route::group( ['middleware' => 'auth'] , function () 
+{	 
+
+	/*
+	* CATEGORIES
+	*/
+	Route::get('/category', 'CategoryController@index' );
+	//ajax
+	Route::get('/api/v1/category', 'CategoryController@getList' );
+	Route::get('/api/v1/category/{id}', 'CategoryController@show' );
+	Route::post('/api/v1/category', 'CategoryController@store' ); 
+	Route::put('/api/v1/category', 'CategoryController@update' ); 
+	Route::delete('/api/v1/category/{id}', 'CategoryController@destroy' );
+
+	/*
+	* PRODUCTS
+	*/
+	Route::get('/product', 'ProductController@index' );
+	//API
+	Route::get('/api/v1/product', 'ProductController@getList' );
+	Route::get('/api/v1/product/{id}', 'ProductController@show' );
+	Route::post('/api/v1/product', 'ProductController@store' ); 
+	Route::put('/api/v1/product', 'ProductController@update' ); 
+	Route::delete('/api/v1/product/{id}', 'ProductController@destroy' );
+
+	/*
+	* USERS *
+	*/
+	Route::get('/user', 'UserController@index' );
+	Route::get('/user/{id}/{name}', 'UserController@show');	
+	//API
+	Route::get('/api/v1/user', 'UserController@index' );
+	Route::post('/api/v1/user', 'UserController@store');
+	Route::put('/api/v1/user/{id}', 'UserController@update');
+	Route::delete('/api/v1/user/{id}', 'UserController@destroy');
+
+
+
+});
+
